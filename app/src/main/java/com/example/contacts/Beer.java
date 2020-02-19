8ef407dfea3053230a9ea76e45fa7692fb19fec1
Majkl <majkl.andracic@gmail.com>
2020-02-19 19:30:20 +0100

    Beer

---
+++ app/src/main/java/com/example/contacts/Beer.java
@@ -0,0 +1,13 @@
+package com.example.contacts;
+
+public class Beer {
+
+    public String name,type;
+    public int imageId;
+
+    public Beer(String name, String type, int imageId) {
+        this.name = name;
+        this.type = type;
+        this.imageId = imageId;
+    }
+}
