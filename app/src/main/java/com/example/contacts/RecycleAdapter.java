9468f8003b7cbd0246e3e91f972f198fcf0bada8
Majkl <majkl.andracic@gmail.com>
2020-02-19 23:58:01 +0100

    Main Activity

--- app/src/main/java/com/example/contacts/RecycleAdapter.java
+++ app/src/main/java/com/example/contacts/RecycleAdapter.java
@@ -1,10 +1,14 @@
 package com.example.contacts;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.contacts.databinding.ItemLayoutBinding;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,14 +26,24 @@ public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.MyViewHo
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+
+        ItemLayoutBinding itemLayoutBinding = DataBindingUtil.inflate(
+                LayoutInflater.from(viewGroup.getContext()), R.layout.item_layout, viewGroup, false);
+
+        MyViewHolder myViewHolder = new MyViewHolder(itemLayoutBinding);
+
+        return myViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder viewHolder, int i) {
 
-    }
 
+
+    Beer beer = beers.get(i);
+    viewHolder.itemLayoutBinding.setBeer(beer);
+
+    }
     @Override
     public int getItemCount() {
         return beers.size();
@@ -37,9 +51,11 @@ public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.MyViewHo
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
 
-        public MyViewHolder(@NonNull View itemView) {
+        ItemLayoutBinding itemLayoutBinding;
+        public MyViewHolder(@NonNull ItemLayoutBinding itemView) {
 
-            super(itemView);
+            super(itemView.getRoot());
+            itemLayoutBinding = itemView;
         }
     }
 }
