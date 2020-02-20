6c5ffd1b4ba1b7612e9086c325b1df709ccc6993
Majkl <majkl.andracic@gmail.com>
2020-02-20 17:55:31 +0100

    Main Activity

--- app/src/main/java/com/example/contacts/MainActivity.java
+++ app/src/main/java/com/example/contacts/MainActivity.java
@@ -9,9 +9,14 @@ import android.os.Bundle;
 
 import com.example.contacts.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding mainBinding;
+    private R
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +26,20 @@ public class MainActivity extends AppCompatActivity {
         mainBinding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
         mainBinding.recyclerView.setHasFixedSize(true);
 
+    }
+    private List<Beer> prepareBeer(){
+
+        List<String> names = Arrays.asList(getResources().getStringArray(R.array.names));
+        List<String> types = Arrays.asList(getResources().getStringArray(R.array.types));
+        int[] imageId = {R.drawable.bura_brew, R.drawable.osjecko, R.drawable.neposlusna, R.drawable.ozujsko,
+                         R.drawable.ozujsko_amber, R.drawable.pan_lager};
+        List<Beer> Names = new ArrayList<>();
+        int count = 0;
+
+        for (String name : names){
+            Names.add(new Beer(name,types.get(count),imageId[count]));
+            count++;
+        }
+        return Names;
     }
 }
